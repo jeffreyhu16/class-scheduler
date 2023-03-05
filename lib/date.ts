af6684258a3f0905bc6b43803aea96c933d029d8
Jeffrey <jeffreyhu16@gmail.com>
2023-03-05 11:47:12 +0800

    add DateTime lib

---
+++ lib/date.ts
@@ -0,0 +1,35 @@
+import { DateTime, Settings } from "luxon";
+
+export const getCurrentDate = () => {
+  return DateTime.local().startOf("day").toObject();
+};
+
+export const getStartOfWeek = () => {
+  return DateTime.local().startOf("week").toObject();
+};
+
+export const getFullWeek = (startOfWeek: Date) => {
+  const monday = DateTime.fromJSDate(startOfWeek);
+
+  return {
+    mon: monday.toObject(),
+    tue: monday.plus({ days: 1 }).toObject(),
+    wed: monday.plus({ days: 2 }).toObject(),
+    thu: monday.plus({ days: 3 }).toObject(),
+    fri: monday.plus({ days: 4 }).toObject(),
+    sat: monday.plus({ days: 5 }).toObject(),
+    sun: monday.plus({ days: 6 }).toObject(),
+  };
+};
+
+export const getTimeOptions = () => {
+  const response = [];
+  let dateTime = DateTime.local().startOf("day").set({ hour: 6 });
+
+  for (let i = 0; i <= 64; i++) {
+    const timeOption = dateTime.toFormat("h:mm a").toLowerCase();
+    response.push(timeOption);
+    dateTime = dateTime.plus({ minutes: 15 });
+  }
+  return response;
+};
