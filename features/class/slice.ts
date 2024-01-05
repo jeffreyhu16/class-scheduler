91625c1994248cee6d3515b1ef3d4346de719de6
Jeffrey <jeffreyhu16@gmail.com>
2024-01-05 18:30:15 +0800

    remove legecy async thunks

--- features/class/slice.ts
+++ features/class/slice.ts
@@ -1,6 +1,5 @@
 import { ClassI } from "@/lib/data/types";
 import { createSlice } from "@reduxjs/toolkit";
-import { getClasses } from "./thunks";
 
 interface ClassState {
   appState: "init" | "loading" | "complete" | "error";
@@ -18,22 +17,6 @@ const classSlice = createSlice({
   name: "class",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(getClasses.pending, (state) => {
-        state.appState = "loading";
-      })
-      .addCase(getClasses.fulfilled, (state, { payload }) => {
-        state.appState = "complete";
-        if (payload?.day != undefined) {
-          state.data[payload.day] = payload?.classes;
-        }
-      })
-      .addCase(getClasses.rejected, (state, { payload }) => {
-        state.appState = "error";
-        state.fetchError = payload as string;
-      });
-  },
 });
 
 export default classSlice.reducer;
